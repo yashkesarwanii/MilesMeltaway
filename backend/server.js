15151fffcad99f6dcbab3b89905cedb89668b731
yashkesarwanii <yashkesarwani6393@gmail.com>
2025-07-27 23:19:07 +0530

    intial commit

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -20,6 +21,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+
+
 
 server.listen(PORT, () => {
 	connectToMongoDB();
