3e3cd49866e5ce0c80e45804296e6931da381ca9
yashkesarwanii <yashkesarwani6393@gmail.com>
2025-07-28 00:43:05 +0530

    4th part completed-socket

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -22,8 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
